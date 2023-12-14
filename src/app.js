92428b1e11030cce6fb83295a23e11f5c6ccf8e2
dimasyoga0810@gmail.com <dimasyoga0810@gmail.com>
2023-12-14 12:08:57 +0700

    mapping codingan

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
-// Get the button:
+//fitur js
+//-button scroll
+//-github stats
+
+
+
+// Variabel pemanggil
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// fungsi saat user scroll website 
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -12,10 +18,10 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// fungsi saat button di klik
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; // di safari
+  document.documentElement.scrollTop = 0; // di chrome Firefox, IE dan opra
 }
 function myFunction() {
    var element = document.body;
@@ -25,8 +31,11 @@ function myFunction() {
    
    
 }
+//fungsi dari github stats
 
+//event
 document.addEventListener('DOMContentLoaded', function () {
+   
     // Ganti dengan username GitHub Anda
     const username = 'dimaskecee';
 
